660bc2a7d5fe240ccc208024d478645cddf5e69e
Tomasz Miotk <thomas.miotk86@gmail.com>
2021-03-21 16:07:05 +0100

    Header Completed 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+      className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 300,
+              },
+            },
+
+            shape: {
+              type: "circle", // stars  v square
+              stroke: {
+                width: 22,
+                color: "black",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
